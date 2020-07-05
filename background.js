7c7275d96275f14e51a4d5209f6c6544a6ed12aa
Yukimi Kazari <dev@9854.moe>
2020-07-04 20:41:36 -0400

    Fixed pageURL Illustrates finder

--- background.js
+++ background.js
@@ -258,7 +258,14 @@ function onClickHandler(info, tab) {
 			// For any non-special downloads
 			function sendDownload() {
 				let sourceLink = info.pageUrl
-				if (info.srcUrl ) { sourceLink = info.srcUrl } else if (info.linkUrl) { sourceLink = info.linkUrl }
+				if (info.srcUrl ) {
+					if (info.srcUrl.includes("youtube.com")) {
+					} else {
+						sourceLink = info.srcUrl
+					}
+				} else if (info.linkUrl) {
+					sourceLink = info.linkUrl
+				}
 				let FileName = sourceLink.split('/').pop()
 				if (FileName.includes(":")) {
 					FileName = FileName.split(":")[0]
